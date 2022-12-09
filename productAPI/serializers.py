723b311c2cd31dbff111ff6eea5d14a16d140c1c
Land of Yeti <robert.beckhusen@aol.com>
2022-12-09 18:24:57 -0500

    product changes

--- productAPI/serializers.py
+++ productAPI/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Product
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
